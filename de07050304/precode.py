4197974c578e8812c377540b849692785d688d5f
Artem Chagin <artem.chgn@yandex.ru>
2023-10-02 15:48:55 +0300

    de07050304 update precode.py

--- de07050304/precode.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession as _SparkSession
-_spark = (
-    _SparkSession
-    .builder
-    .config('spark.ui.showConsoleProgress', 'false')
-    .getOrCreate()
-)
-_spark.sparkContext.setLogLevel('OFF')
